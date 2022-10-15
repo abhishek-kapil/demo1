d81ca47503ec2c8b83a175d6e9e6239f34cb1e29
abhishek-kapil <abhishekkapil.ak@gmail.com>
2022-10-15 15:31:38 +0530

    done

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello{
       public static void main(String args[]){
          int a=10;
          int b=25;
-         System.out.println("Hello world"+(a+b));
+         System.out.println("Hello world after commiting changes " +(a+b));
       }
 }
